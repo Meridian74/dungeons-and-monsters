678d7e7c4dd9c9d4fe88296e5129dd3d6f76ded6
Meridian74 <meridian@freemail.hu>
2023-01-31 17:28:34 +0100

    Fixed stucking into a wall element

--- src/main/java/meridian/map/CollisionChecker.java
+++ src/main/java/meridian/map/CollisionChecker.java
@@ -18,105 +18,115 @@ public class CollisionChecker {
    }
 
 
+   /**
+    * Since the movement consists of small partial positions and it is possible to go in
+    * two main directions at the same time (in the case of diagonal movement), several cells
+    * must be checked to see if they block free movement or not... if the cell is solid in
+    * one of the main directions (vertically or horizontally), then it is false returns
+    * with a value that prevents the entity from moving further in that direction. If a cell
+    * is traversable, it returns true, and the entity moves in that direction.
+    *
+    * @param entity a player or a monster.
+    * @param direction main direction of displacement under investigation.
+    * @return boolean 'true': can move -- 'false': cannot move in that direction.
+    */
    public boolean canMove(Entity entity, Direction direction) {
-      boolean result = true;
 
       int entityPosX = entity.getWorldCol();
       int entityPosY = entity.getWorldRow();
       int shiftX = entity.getShiftX();
       int shiftY = entity.getShiftY();
+
       int nextPosX = entityPosX;
-      int nextAfterPosX = entityPosX;
       int nextPosY = entityPosY;
-      int nextAfterPosY = entityPosY;
-
-
-      MapCell[][] cells = mapManager.getCells();
+      int neighbourPosX = entityPosX;
+      int neighbourPosY = entityPosY;
 
       switch (direction) {
          case UP -> {
             if (shiftY >= 0) {
                nextPosY--;
-            }
-
-            if (shiftX > 0 && shiftY >= 0) {
-               nextAfterPosX--;
-            }
-            else if (shiftX < 0 && shiftY >= 0) {
-               nextAfterPosX++;
+               neighbourPosY--;
             }
 
             if (nextPosY < mapManager.getWorldTop()) {
-               result = false;
+               return false;
+            }
+
+            if (shiftX < 0) {
+               neighbourPosX++;
+            }
+            else if (shiftX > 0) {
+               neighbourPosX--;
             }
          }
          case DOWN -> {
-            if (shiftX <= 0) {
+            if (shiftY <= 0) {
                nextPosY++;
-            }
-
-            if (shiftX > 0 && shiftY <= 0) {
-               nextAfterPosX--;
-            }
-            else if (shiftX < 0 && shiftY <= 0) {
-               nextAfterPosX++;
+               neighbourPosY++;
             }
 
             if (nextPosY >= mapManager.getWorldBottom()) {
-               result = false;
+               return false;
+            }
+
+            if (shiftX < 0) {
+               neighbourPosX++;
+            }
+            else if (shiftX > 0) {
+               neighbourPosX--;
             }
          }
          case LEFT -> {
             if (shiftX >= 0) {
                nextPosX--;
-            }
-
-            if (shiftY > 0 && shiftX >= 0) {
-               nextAfterPosY--;
-            }
-            else if (shiftY < 0 && shiftX >= 0) {
-               nextAfterPosY++;
+               neighbourPosX--;
             }
 
             if (nextPosX < mapManager.getWorldLeft()) {
-               result = false;
+               return false;
+            }
+
+            if (shiftY < 0) {
+               neighbourPosY++;
+            }
+            else if (shiftY > 0) {
+               neighbourPosY--;
             }
          }
          case RIGHT -> {
             if (shiftX <= 0) {
                nextPosX++;
-               nextAfterPosX++;
+               neighbourPosX++;
             }
-
-            if (shiftY > 0 && shiftX <= 0) {
-               nextAfterPosY--;
-            }
-            else if (shiftY < 0 && shiftX <= 0) {
-               nextAfterPosY++;
-            }
-
             if (nextPosX >= mapManager.getWorldRight()) {
-               result = false;
+               return false;
+            }
+
+            if (shiftY < 0) {
+               neighbourPosY++;
+            }
+            else if (shiftY > 0) {
+               neighbourPosY--;
             }
          }
       }
 
+      // Check in the direction of main move
+      boolean result = isCellTraverseable(nextPosX, nextPosY);
 
+      // Check in the direction diagonally as well.
       if (result) {
-         Tile tile = cells[nextPosY][nextPosX].getTile();
-         if (tile.isSolid()) {
-            result = false;
-         }
+         result = isCellTraverseable(neighbourPosX, neighbourPosY);
       }
-      if (result) {
-         Tile tile = cells[nextAfterPosY][nextAfterPosX].getTile();
-         if (tile.isSolid()) {
-            result = false;
-         }
-      }
-
 
       return result;
+   }
+
+   private boolean isCellTraverseable(int nextPosX, int nextPosY) {
+      MapCell[][] cells = this.mapManager.getCells();
+      Tile tile = cells[nextPosY][nextPosX].getTile();
+      return !tile.isSolid();
    }
 
 }
